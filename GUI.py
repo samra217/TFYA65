1bd9fadbaf860ea2a45e69bb6208f6a0dce07fde
samra217 <samra217@student.liu.se>
2025-09-25 11:03:53 +0200

    Update GUI.py

--- GUI.py
+++ GUI.py
@@ -2,9 +2,20 @@ import os
 import tensorflow as tf
 from tensorflow import keras
 from tkinter import *
+import numpy as np
 
 import pyaudio
 
+SAMPLING_RATE = 16000
+
+RUNNING = True
+
+
+
+model = keras.models.load_model("TFYA65.keras")
+class_names = os.listdir("16000_pcm_speeches/audio")
+
+p = pyaudio.PyAudio()
 root = Tk()
 root.geometry("200x150")
 frame = Frame(root)
@@ -14,12 +25,56 @@ frame.pack()
 label = Label(frame, text = "hello world!")
 label.pack()
 
+
+def on_closing():
+    root.destroy()
+    RUNNING = False
+    
+root.protocol("WM_DELETE_WINDOW", on_closing)
 root.title("Test")
+
+
+
+
+mic_stream = p.open(format = pyaudio.paFloat32, rate=SAMPLING_RATE, channels =1, input=True, frames_per_buffer=SAMPLING_RATE)
+
+def audio_to_fft(audio):
+    audio = tf.squeeze(audio, axis=-1)
+    fft = tf.signal.fft(
+        tf.cast(tf.complex(real=audio, imag=tf.zeros_like(audio)), tf.complex64)
+    )
+    fft = tf.expand_dims(fft, axis=-1)
+    return tf.math.abs(fft[:, : (audio.shape[1] // 2), :])
+
+
+def prediction():
+    audio_bytes = mic_stream.read(SAMPLING_RATE, exception_on_overflow=False)
+    audio_np = np.frombuffer(audio_bytes, dtype=np.float32)
+
+    
+    audio_np = audio_np.reshape(-1,1)
+    audio_tensor = tf.convert_to_tensor([audio_np], dtype=tf.float32)
+    fft = audio_to_fft(audio_tensor)
+
+    pred = model.predict(fft)[0]
+
+    probs = pred * 100  
+    sorted_idx = np.argsort(probs)[::-1]
+
+    print("Sannolikheter:")
+    for idx in sorted_idx:
+        print(f"{class_names[idx]}: {probs[idx]:.2f}%")
+
+    
+    root.after(3000, prediction)
+    
+
+root.after(3000, prediction)
+
+
 root.mainloop()
-
-
-mic_stream = pyaudio.PyAudio.open(rate=16000, channels =1, input=True, frames_per_buffer=16000)
-
+mic_stream.stop_stream()
 mic_stream.close()
+p.terminate()
 
 
